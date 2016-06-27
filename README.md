6763f60194c44286aa3aae021b2cd77f6e32bd54
Josie GD <theblackophelia@gmail.com>
2016-06-28 09:29:55 +1000

    new pe license check

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Spot to put my puppet nagios checks.  Available for use 'as-is'.
 
 ## checks/check_puppet_nodes.py
 
-Checks the status of a puppet environmnet, providing a summary or detailed version of the current state.
+Checks the status of a puppet environment, providing a summary or detailed version of the current state.
 
 Usage:
 ```
@@ -31,6 +31,8 @@ Options:
   -m MAX_HOSTS, --max-hosts=MAX_HOSTS
                         Maximum number of host names to display per state.
                         Default is 5.
+  -W, --warn-failed     Failed nodes won't trigger critical state.
+  -C, --crit-failed     Failed nodes will trigger critical state. Default.
 ```
 
 Sample output from `summary` mode:
@@ -43,3 +45,33 @@ Sample output from `detailed` mode:
 ```
 CRITICAL:: Total nodes=23, Changed=2 host-01 host-02, Failed=1 test-host-01, Noop=0, Unchanged=20 dev-host-01 prod-host-01 dev-host-02 dev-host-03 prod-host-02..., Unreported=0, Unresponsive=0,
 ```
+
+## checks/check_pe_license.sh
+
+Checks the status of  a PE 2015.x, PE 2016.x installation and licensing.  Designed to run locally on the puppetmaster as an NRPE Nagios check.
+
+Usage:
+```
+  Script to check PE license status, for number of nodes remaining under license and number of days before expiry.
+  Assumes that the host on which this script is run is the puppetmaster.  Designed to run as an NRPE check, from nagios.
+
+  usage: ./check_pe_license.sh [-h] [-w num] [-c num] [-W num] [-C num]
+
+    -h            Usage.
+    -w            Number of nodes remaining before a warning alert is triggered.  Default is 15.
+    -c            Number of nodes remaining before a critical alert is triggered.  Default is 5.
+    -W            Number of days before license expiry before a warning alert is triggered.  Default is 15.
+    -C            Number of days before license expiry before a critical alert is triggered.  Default is 5.
+
+  Returns standard Nagios return codes:
+  - 0 for success
+  - 1 for warning
+  - 2 for critical
+  - 3 for unknown
+```
+
+Sample output:
+```
+OK: Puppet Enterprise License:: Nodes used 8/150. License expires in 167 days, on 2016-12-13.
+```
+
--- checks/check_puppet_nodes.py
+++ checks/check_puppet_nodes.py
@@ -2,8 +2,8 @@
 
 #############################################################################
 #                                                                           #
-# This script was initially developed by Infoxchange for internal use       #
-# and has kindly been made available to the Open Source community for       #
+# This script was initially developed by Josie Gioffre for internal use     #
+# at IX and has kindly been made available to the Open Source community for #
 # redistribution and further development under the terms of the             #
 # GNU General Public License v3: http://www.gnu.org/licenses/gpl.html       #
 #                                                                           #
@@ -67,7 +67,11 @@ def get_base_url():
 def status():
     state = ''
     exit = OK;
+
     if node_status_totals[FAILED] + node_status_totals[UNRESPONSIVE] + node_status_totals[UNREPORTED]  >=  options.critical_threshold:
+        status = 'CRITICAL:'
+        exit = CRITICAL
+    elif node_status_totals[FAILED] > 0 and options.critical_on_failed:
         status = 'CRITICAL:'
         exit = CRITICAL
     elif node_status_totals[FAILED] + node_status_totals[UNRESPONSIVE] + node_status_totals[UNREPORTED]  >=  options.warning_threshold:
@@ -132,11 +136,13 @@ if __name__ == '__main__':
     parser.add_option("-H", "--hostname", action="store", dest="hostname", type="string", help="The PuppetDB hostname (fqdn). eg: dev-puppetmaster-01.serv.abb.ixa.net.au")
     parser.add_option("-p", "--port", action="store", dest="port", type="int", default=8080, help="The PuppetDB port. Defaults to 8080")
     parser.add_option("-s", "--summary", action="store_false", dest="detailed",  help="Print summary - just the numbers (not host details)")
-    parser.add_option("-d", "--detailed", action="store_true", dest="detailed", default=True, help="Print details - inlcudes host details")
-    parser.add_option("-w", "--warning", action="store", dest="warning_threshold", type="int", default="1", help="Warning if <num> hosts are failing.  Defaults to 1.")
-    parser.add_option("-c", "--critical", action="store", dest="critical_threshold", type="int", default="4", help="Critical if <num> hosts are failing.  Defaults to 4.")
+    parser.add_option("-d", "--detailed", action="store_true", dest="detailed", default=True, help="Print details - includes host details.  Default.")
+    parser.add_option("-w", "--warning", action="store", dest="warning_threshold", type="int", default="1", help="Warning if <num> hosts are failing/unresponsive/unreported.  Defaults to 1.")
+    parser.add_option("-c", "--critical", action="store", dest="critical_threshold", type="int", default="4", help="Critical if <num> hosts are failing/unresponsive/unreported.  Defaults to 4.")
     parser.add_option("-f", "--fqdn", action="store_true", dest="fqdn_hostnames",  help="Show FQDN for hostname, instead of short hostnames.  Default is short hostname.")
     parser.add_option("-m", "--max-hosts", action="store", dest="max_hosts", type="int", default="5", help="Maximum number of host names to display per state.  Default is 5.")
+    parser.add_option("-W", "--warn-failed", action="store_false", dest="critical_on_failed", help="Failed nodes won't trigger critical state.")
+    parser.add_option("-C", "--crit-failed", action="store_true", dest="critical_on_failed", help="Failed nodes will trigger critical state. Default.")
 
     (options, args) = parser.parse_args()
     if not options.hostname:   # if filename is not given
